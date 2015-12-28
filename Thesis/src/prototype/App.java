aba3ab33e9bebb5c208f253e7264a5c89d905701
Eihwaz <s.setsuna@yahoo.com>
2015-12-29 00:03:28 +0800

    12:03 am (12/19/15)

--- Thesis/src/prototype/App.java
+++ Thesis/src/prototype/App.java
@@ -4,9 +4,8 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi po");
-		
-		System.out.println("test git");
+		MainFrame frame = new MainFrame("Prototype");
+		frame.setVisible(true);
 	}
 
 }
--- Thesis/src/prototype/MainFrame.java
+++ Thesis/src/prototype/MainFrame.java
@@ -1,8 +1,29 @@
 package prototype;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
 
 public class MainFrame 
 	extends JFrame {
 
+	JPanel panel = new JPanel(new MigLayout(""));
+	JLabel lblLoadModel = new JLabel("Load Model");
+	JLabel lblSetLabel = new JLabel("Set Label");
+	JList myList = new JList();
+	
+	public MainFrame(String text){
+		super(text);
+		
+		
+	}
+	
+	void initPanel(){
+		panel.add(lblLoadModel);
+		panel.add(lblSetLabel);
+		
+	}
 }
